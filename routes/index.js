417dec4d07e73a3ff607849b75a7bd054122ee7c
DEPA HARSHITHA <23eg107b16@anurag.edu.in>
2025-08-20 12:15:42 +0530

    Updated index route, homepage view, and styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Careercompasss' });
+  res.render('index', { 
+    title: 'Careercompass',
+    pages: [
+      "Login / Signup",
+      "Landing Page",
+      "Dashboard",
+      "Career Roadmap",
+      "Resume Tools",
+      "About Us",
+      "Contact / Help"
+    ]
+  });
 });
 
 module.exports = router;
